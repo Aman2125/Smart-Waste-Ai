04a27baac43790775646b8979536e1908f9ecd9f
Aman2125 <amankumar943087@gmail.com>
2026-04-19 22:26:52 +0530

    gpu trained .pt used

--- src/train_gpu.py
+++ src/train_gpu.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("yolov8s.pt")  # better model for GPU
+    model = YOLO("yolov8s.pt")  # this is used after trained on google colab for 50 epoch on complete dataset
+    # after training this one is used directly here
 
     model.train(
         data="data/YOLO-Waste-Detection-1/data.yaml",
